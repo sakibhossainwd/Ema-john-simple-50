0f930245864a4a65fd735b573b298bb799216544
Sakib Hossain <sakibhossainceo.com>
2023-10-25 23:05:20 +0600

    shop file added

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='Shop-Container'>
+            <div className="prducts-container">
+
+            </div>
+            <div className="card-container">
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;

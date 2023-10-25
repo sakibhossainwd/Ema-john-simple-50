06239c3f3b04f95240519c454afd24f6f01c07b0
Sakib Hossain <sakibhossainceo.com>
2023-10-25 23:54:48 +0600

    product img added and take in the grid

--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import './Products.css'
 
 const Products = (props) => {
     const {img, name, seller, price} = props.product;
     console.log(props)
     return (
-        <div>
+        <div className='product'>
             <img src={img} alt="" />
         </div>
     );

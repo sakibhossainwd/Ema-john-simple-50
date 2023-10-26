8e85132b591f8114abc8633eaa87f2a452a42139
Sakib Hossain <sakibhossainceo.com>
2023-10-26 13:53:21 +0600

    card added

--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 import './Products.css'
 
 const Products = (props) => {
-    const {img, name, seller, price} = props.product;
+    const {img, name, seller, price, ratings} = props.product;
     console.log(props)
     return (
         <div className='product'>
             <img src={img} alt="" />
+                <div className="product-details">
+                <div className="name-price">
+                    <h3>{name}</h3>
+                    <p>Price: ${price}</p>
+                </div>
+                <div className="brand-rating">
+                    <p>Manufecture: {seller}</p>
+                    <p>Ratting: {ratings} Start</p>
+                </div>
+                </div>
         </div>
     );
 };

143c52d7e560424988bf773af39c77b55d7e3c7e
Sakib Hossain <sakibhossainceo.com>
2023-11-03 09:17:55 +0600

    show date from localstorage in the cart calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -16,10 +16,12 @@ const Cart = ({cart}) => {
     // console.log(cart)
     let total = 0;
     let shipping = 0;
-
+    let quantity = 0;
     for (const product of cart){
-       total = total + product.price;
-       shipping = shipping + product.shipping;
+       product.quantity = product.quantity || 1;
+       total = total + product.price * product.quantity;
+       shipping = shipping + product.shipping * product.quantity;
+       quantity = quantity + product.quantity;
     }
     const tax = total*7/100
     
@@ -31,7 +33,7 @@ const Cart = ({cart}) => {
              <div className="heading">
                  <h2>Order Summary</h2>
              </div>
-            <p>Product items: {cart.length}</p>
+            <p>Product items: {quantity}</p>
             <p>Total Price: $ {total.toFixed(2)}</p>
             <p>Toal Shiping Charge: ${shipping.toFixed(2)}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
